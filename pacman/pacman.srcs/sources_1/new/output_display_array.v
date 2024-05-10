b979fe6f55a02852ce3195794a33601f376ba0b4
BingYu_Li <bingyuli@Bings-MacBook-Air.local>
2024-05-10 20:25:19 +0800

    update display

--- pacman/pacman.srcs/sources_1/new/output_display_array.v
+++ pacman/pacman.srcs/sources_1/new/output_display_array.v
@@ -4,3 +4,20 @@ module output_display_array(
 
     );
 endmodule
+`timescale 1ns / 1ps
+
+`define WIDTH 1280
+`define HEIGHT 720
+
+module output_display_array(
+    output [3:0] r_720p[0:'HEIGHT-1][0:'WIDTH-1],
+    output [3:0] g_720p[0:'HEIGHT-1][0:'WIDTH-1],
+    output [3:0] b_720p[0:'HEIGHT-1][0:'WIDTH-1]
+    );
+
+    reg [3:0] r[0:'HEIGHT-1][0:'WIDTH-1];
+    reg [3:0] g[0:'HEIGHT-1][0:'WIDTH-1];
+    reg [3:0] b[0:'HEIGHT-1][0:'WIDTH-1];
+
+
+endmodule
