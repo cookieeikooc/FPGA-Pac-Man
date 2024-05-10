969354e1d8d1c8d63464a5f3ad2bf220dc96f923
BingYu_Li <bingyuli@Bings-MacBook-Air.local>
2024-05-10 20:07:19 +0800

    update output_diaplay_array.v

--- pacman/pacman.srcs/sources_1/new/output_display_array.v
+++ pacman/pacman.srcs/sources_1/new/output_display_array.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/10/2024 07:47:47 PM
-// Design Name: 
-// Module Name: output_display_array
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module output_display_array(
 
