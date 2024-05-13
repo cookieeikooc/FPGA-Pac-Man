dd934a9d2c711024a14c2519e61f3b9eefdcf3ef
BingYu_Li <bingyuli@Bings-MacBook-Air.local>
2024-05-13 14:54:01 +0800

    update display_cache

--- pacman/pacman.srcs/sources_1/new/display_cache.v
+++ pacman/pacman.srcs/sources_1/new/display_cache.v
@@ -1,14 +1,19 @@
-/*
+`define WIDTH 1280
+`define HEIGHT 720
 
-Map Hight: 248
-Map Width: 224
-Path Width: 16
-Pac-Man: 13 * 13
-Ghost: 14 * 14
-*/
+module display_cache (
+    input [7:0]current_px_row,
+    input [7:0]current_px_col,
+    output [11:0] rgb_720p,
 
-module display_template (
-    
+    //for map_template
+    input [11:0] map_rgb,
+    output [7:0]current_tile_row,
+    output [7:0]current_tile_col,
+    output [2:0]current_tile_px_row,
+    output [2:0]current_tile_px_col
+
+    //
 ):
 
 endmodule
