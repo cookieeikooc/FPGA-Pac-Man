12b113b6318a50c668ea6f4fa7f9ae0a17246cc0
BingYu_Li <bingyuli@Bings-MacBook-Air.local>
2024-05-17 04:03:55 +0800

    add pacman ROM

---
+++ pacman/pacman.srcs/sources_1/new/board/pacman_ROM.v
@@ -0,0 +1,12 @@
+// Bing
+
+module pacman_ROM (input [3:0] tile_px_row,
+    input [3:0] tile_px_col,
+    input [1:0] facing,
+    input [1:0]frame, //0 closed, 1 open, 2 wide open
+    output [11:0] rgb
+);
+
+    
+
+endmodule
