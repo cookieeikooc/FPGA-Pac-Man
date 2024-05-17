4e4db8f364f2c30b566e94fdbdb91a9af97913f3
BingYu_Li <bingyuli@Bings-MacBook-Air.local>
2024-05-17 20:11:08 +0800

    pacman ROM

--- pacman/pacman.srcs/sources_1/new/board/pacman_ROM.v
+++ pacman/pacman.srcs/sources_1/new/board/pacman_ROM.v
@@ -2,13 +2,13 @@
 
 module pacman_ROM (input [3:0] tile_px_row,
     input [3:0] tile_px_col,
-    input [1:0]frame //0: Closed 1: R wide open 2: R open 3: D wide open
+    input [4:0]frame //0: Closed 1: R wide open 2: R open 3: D wide open
                      //4: D open 5: L wide open 6: L open 7: U wide open
                      //8: U open 9: Die 1       21: Die 13
     output [11:0] rgb
 );
 
-    reg ROM [0:1408]:
+    reg ROM [0:5631]:
     initial begin
         $readmemh("pacman_ROM.mem", ROM);
     end
