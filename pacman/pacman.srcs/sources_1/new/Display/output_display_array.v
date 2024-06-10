fbf8f9ea29c68cfc375dfc7ec6ab5edfbae345eb
BingYu_Li <bingyuli@Bings-MacBook-Air.local>
2024-06-10 19:44:56 +0800

    change memory method

--- pacman/pacman.srcs/sources_1/new/Display/output_display_array.v
+++ pacman/pacman.srcs/sources_1/new/Display/output_display_array.v
@@ -45,7 +45,7 @@ module output_display_array (
     );
 
     //Read Cache and Scale
-    reg [11:0] rgb[0:287][0:223];
+    (*ram_style = "block") reg [11:0] rgb[0:287][0:223];
     always @(posedge clk) begin
         if (board_ready == 1'b1 & row >= 10'd720) begin
             rgb[24 + board_px_row_counter][board_px_col_counter] <= board_rgb;
